3417b363e6e90aa1fd686e2f0021365d3d85ee71
Valdo <vlorian22@GMAIL.COM>
2020-10-24 22:10:01 -0500

    added question form to index.html

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,20 @@
-const beginQuiz =document.getElementById("begin");
+const beginQuiz =document.getElementById("button");
 const time=document.getElementById("countdown");
 const totalScore=document.getElementById("header");
+let quizContent =document.getElementById("question");
+const A=document.getElementById("A");
+const B=document.getElementById("B");
+const C=document.getElementById("C");
+const D= document.getElementById("D");
 
  let questions = [{
      question:"What does HTML stand for?",
      a:"??",
+     b:"??",
      c:"??1",
      d:"?? 1",
      correct:"A"
-
- },
+    },
  {
      question:"What do you type first in html?",
      a:"??",
@@ -28,3 +33,23 @@ const totalScore=document.getElementById("header");
  }
       
  ]
+
+ 
+
+ const indexQuestion= questions.length-1;
+ let dashQuestion=0;
+
+ function sortQuestion(){
+    //  loaded question array holder to sort through object array
+     let lqah=questions[dashQuestion];
+     quizContent.innerHTML=lqah.question;
+     A.innerHTML=lqah.a;
+     B.innerHTML=lqah.b;
+     C.innerHTML=lqah.c;
+     D.innerHTML=lqah.d;
+ }
+
+
+ sortQuestion();
+
+
