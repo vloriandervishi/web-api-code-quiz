6801fbd0efc65f9f8270675e7b8ca0c7c9272664
Valdo <vlorian22@GMAIL.COM>
2020-10-24 20:11:36 -0500

    Setup index html and css and scritpt

---
+++ assets/script/script.js
@@ -0,0 +1,30 @@
+const beginQuiz =document.getElementById("begin");
+const time=document.getElementById("countdown");
+const totalScore=document.getElementById("header");
+
+ let questions = [{
+     question:"What does HTML stand for?",
+     a:"??",
+     c:"??1",
+     d:"?? 1",
+     correct:"A"
+
+ },
+ {
+     question:"What do you type first in html?",
+     a:"??",
+     b:"??",
+     c:"??",
+     d:"??",
+     correct:"C" 
+ },
+ {
+     question:"What was the l",
+     a:"??",
+     b:"??",
+     c:"??",
+     d:"??",
+     correct:"B"
+ }
+      
+ ]
