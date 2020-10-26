751dd42728a2ff65f1d36cc91e9d226d39ec1048
Valdo <vlorian22@GMAIL.COM>
2020-10-25 22:30:51 -0500

    interactive click on the questions

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,58 +1,81 @@
 const beginQuiz =document.getElementById("button");
 const time=document.getElementById("countdown");
 const totalScore=document.getElementById("header");
-let quizContent =document.getElementById("question");
-const A=document.getElementById("A");
-const B=document.getElementById("B");
-const C=document.getElementById("C");
-const D= document.getElementById("D");
-
+const question =document.getElementById("question");
+const choices=Array.from(document.getElementsByClassName("choice-text"));
+// const A=document.getElementById("A");
+// const B=document.getElementById("B");
+// const C=document.getElementById("C");
+// const D=document.getElementById("D");
  let questions = [{
-     question:"What does HTML stand for?",
-     a:"A.first question",
-     b:"B.??",
-     c:"C.??1",
-     d:"D.?? 1",
-     correct:"??"
+     question:"How can you make a list that lists the items with numbers?",
+     a:"ol",
+     b:"DI",
+     c:"UI",
+     d:"List",
+     answer:"a"
     },
  {
-     question:"What do you type first in html?",
-     a:"??",
-     b:"??",
-     c:"??",
-     d:"??",
-     correct:"??" 
+     question:"Who is making the Web Standards?",
+     a:"Mozilla",
+     b:"Microsoft",
+     c:"Apple",
+     d:"The world wide web consortium",
+     answer:"d" 
  },
  {
-     question:"What was the l",
-     a:"??",
-     b:"??",
-     c:"??",
-     d:"??",
-     correct:"??"
+     question:"Choose the correct HTML tag for the largest heading.",
+     a:"H1",
+     b:"Heading",
+     c:"head",
+     d:"h6",
+     answer:"a"
  }
       
  ]
-
- 
-
- const indexQuestion= questions.length-1;
- let dashQuestion=0;
-
- function sortQuestion(){
-    //  loaded question array holder to sort through object array
-     
-    let lqah=questions[dashQuestion];
-     
-     quizContent.innerHTML=lqah.question;
-     A.innerText=lqah.a;
-     B.innerHTML=lqah.b;
-     C.innerHTML=lqah.c;
-     D.innerHTML=lqah.d;
-    
+ const endOfQuestion= 3;
+ let currentQuestion ={};
+ let acceptingAnwers=false;
+ let score=0;
+ let questionCounter=0;
+ let availableQuestions=[];
+ startGame=()=>{
+  questionCounter=0;
+  score=0;
+  availableQuestions=[...questions];
+ // console.log(availableQuestions);
+  getNextQstn();
  }
- sortQuestion();
-
+ getNextQstn=()=> {
+     if(availableQuestions.length===0 || questionCounter>endOfQuestion)
+     {
+         return window.location.assign('/end.html')
+     }
+     questionCounter++;
+     currentQuestion=availableQuestions[Math.floor(Math.random()*availableQuestions.length)];
+     question.innerHTML=currentQuestion.question;
+     
+     choices.forEach(choice=>{
+       const letter=choice.dataset["letter"];
+       choice.innerHTML=currentQuestion[letter];
+     });
+     availableQuestions.splice(currentQuestion,1);
+     acceptingAnwers=true;
+ };
+ choices.forEach(choice=>{
+     choice.addEventListener("click",e=>{
+         if(!acceptingAnwers) return;
+         acceptingAnwers=false;
+         const displayedQuestion= e.target;
+         const chosenAnswer=displayedQuestion.dataset["letter"];
+         
+         const classToApply = chosenAnswer==currentQuestion.answer? "correct": "incorrect";
+        
+        
+         getNextQstn();
+     });
+ })
+ startGame();
  
 
 
