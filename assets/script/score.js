3a342254e6721f6ffcc500ff3c345748c421eed2
Valdo <vlorian22@GMAIL.COM>
2020-12-01 14:03:32 -0600

    got to display score and name on the screen

--- assets/script/score.js
+++ assets/script/score.js
@@ -1,26 +1,47 @@
-var getName=document.getElementById("getName").value;
-var hScore=JSON.parse(localStorage.getItem('score'))||[];
-
-// get saved score from script.js and 
+var getName = document.getElementById("getName").value;
+var hScore = JSON.parse(localStorage.getItem("score")) || [];
+const ArrayObject = [];
+// get saved score from script.js and
 // store it into hScore
-// Have after quiz finished to display the score and then prompt to enter name 
+// Have after quiz finished to display the score and then prompt to enter name
 //save score to that name.
-const showScore=document.getElementsByClassName('printScore').innerHTML=hScore;
-//document.body.append(showScore);
+const showScore = document.getElementById(
+  "printScore"
+).innerHTML = 'Final Score:' + hScore;
 
-function getInput(){
-    var getName=document.getElementById("getName").value;
-    document.getElementById('demo').innerHTML='You Final Score: ' + getName +' '+ showScore;
-    
-    if(document.querySelector('#divBox').style.display==="none" && document.querySelector('#viewScore').style.display==="none"){
-        document.querySelector('#divBox').style.display= "block";
-        document.querySelector('#viewScore').style.display="block";
-    }
-    else {
-        document.querySelector('#divBox').style.display="none";
-        document.querySelector('#viewScore').style.display="none";
-       
-    }
-    
 
+function getInput() {
+  var getName = document.getElementById("getName").value;
+  document.getElementById("demo").innerHTML =
+    "HighScore: ";
+    localStorage.setItem(JSON.stringify(getName),JSON.stringify(hScore));
+    
+      var flex= document.getElementById('flex');
+       var div= document.createElement('div');
+       flex.appendChild(div);
+       var ol =document.createElement('ol');
+       var li= document.createElement('li');
+       div.setAttribute('id','order');
+       div.appendChild(ol);
+       ol.appendChild(li);
+       li.appendChild(document.createTextNode(getName));
+       li.appendChild(document.createTextNode(' '));
+       li.appendChild(document.createTextNode(hScore));
+    
+  if (
+    document.querySelector("#divBox").style.display === "none" &&
+    document.querySelector("#viewScore").style.display === "none" && document.querySelector("#printScore").style.display === "none" 
+  ) {
+    document.querySelector("#divBox").style.display = "block";
+    document.querySelector("#viewScore").style.display = "block";
+    document.querySelector("#printScore").style.display = "block" 
+   
+  } else {
+    document.querySelector("#divBox").style.display = "none";
+    document.querySelector("#viewScore").style.display = "none";
+    document.querySelector("#printScore").style.display = "none" 
+    
+  }
+
+  
 }
--- assets/script/script.js
+++ assets/script/script.js
@@ -4,32 +4,32 @@ const totalScore = document.getElementById("header");
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
-let questions = [{
+let questions = [
+  {
     question: "How can you make a list that lists the items with numbers?",
     a: "ol",
     b: "DI",
     c: "UI",
     d: "List",
-    answer: "a"
-},
-{
+    answer: "a",
+  },
+  {
     question: "Who is making the Web Standards?",
     a: "Mozilla",
     b: "Microsoft",
     c: "Apple",
     d: "The world wide web consortium",
-    answer: "d"
-},
-{
+    answer: "d",
+  },
+  {
     question: "Choose the correct HTML tag for the largest heading.",
     a: "H1",
     b: "Heading",
     c: "head",
     d: "h6",
-    answer: "a"
-}
-
-]
+    answer: "a",
+  },
+];
 const endOfQuestion = 3;
 let currentQuestion = {};
 let acceptingAnwers = false;
@@ -37,88 +37,79 @@ let score = 0;
 let questionCounter = 0;
 let remainingQuestions = [];
 
-function countdown(){
-    time.textContent='';
-    var timeLeft= 30;
-    var timeInterval=setInterval(function(){
-        if(timeLeft>1){
-            time.textContent=timeLeft+ 'seconds remianing';
-            timeLeft--;
-        }else if(timeLeft===1){
-            time.textContent=timeLeft + 'seconds remaining';
-           
-            timeLeft--;
-            
-        }else {
-            time.textContent=" ";
-            clearInterval(timeInterval);
-            
-        }
-    },1000);
+function countdown() {
+  time.textContent = "";
+  var timeLeft = 30;
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      time.textContent = timeLeft + "seconds remianing";
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      time.textContent = timeLeft + "seconds remaining";
+
+      timeLeft--;
+    } else {
+      time.textContent = " ";
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 }
 
 startQuiz = () => {
-   
-    questionCounter = 0;
-    score = 0;
-    remainingQuestions = [...questions];
-    // console.log(availableQuestions);
-    getNextQstn();
-}
-getNextQstn = () => {
-     questionCounter++;
-    if (questionCounter > endOfQuestion) {
-        localStorage.setItem('score',score);
-       
-        return window.location.assign('/yourHighScore.html');
-        
-    }
-   
-    currentQuestion = remainingQuestions[questionCounter-1];
-    question.innerHTML = currentQuestion.question;
-
-    choices.forEach(choice => {
-        const letter = choice.dataset["letter"];
-        choice.innerHTML = currentQuestion[letter];
-    });
-   // remainingQuestions.splice(currentQuestion, 1);
-    acceptingAnwers = true;
+  questionCounter = 0;
+  score = 0;
+  remainingQuestions = [...questions];
+  // console.log(availableQuestions);
+  getNextQstn();
 };
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if (!acceptingAnwers) return;
-        acceptingAnwers = false;
-        const displayedQuestion = e.target;
-        const chosenAnswer = displayedQuestion.dataset["letter"];
-        //const classToApply = chosenAnswer == currentQuestion ? "correct" : "wrong";
-        //classToApply.
-        if(chosenAnswer==currentQuestion.answer){
-            // alert("correct");
-              var correct=document.querySelector(".correct");
-              correct.textContent=" Correct!";
-              score++;
-              console.log(score);
-            //console.log("correct");
-        }else{
-            //alert("wrong");
-           // console.log("wrong");
-           var wrong=document.querySelector(".incorrect");
-           wrong.textContent="Wrong!";
-        }
-        setTimeout(()=>{
-            var correct=document.querySelector(".correct");
-            var wrong=document.querySelector(".incorrect");
-         correct.textContent="";
-         wrong.textContent="";
-        },1500);
-        
-        
-         getNextQstn();
-    });
-})
-onclick=countdown();
+getNextQstn = () => {
+  questionCounter++;
+  if (questionCounter > endOfQuestion) {
+    localStorage.setItem("score", score);
+
+    return window.location.assign("/yourHighScore.html");
+  }
+
+  currentQuestion = remainingQuestions[questionCounter - 1];
+  question.innerHTML = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const letter = choice.dataset["letter"];
+    choice.innerHTML = currentQuestion[letter];
+  });
+  // remainingQuestions.splice(currentQuestion, 1);
+  acceptingAnwers = true;
+};
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnwers) return;
+    acceptingAnwers = false;
+    const displayedQuestion = e.target;
+    const chosenAnswer = displayedQuestion.dataset["letter"];
+    //const classToApply = chosenAnswer == currentQuestion ? "correct" : "wrong";
+    //classToApply.
+    if (chosenAnswer == currentQuestion.answer) {
+      // alert("correct");
+      var correct = document.querySelector(".correct");
+      correct.textContent = " Correct!";
+      score++;
+      console.log(score);
+      //console.log("correct");
+    } else {
+      //alert("wrong");
+      // console.log("wrong");
+      var wrong = document.querySelector(".incorrect");
+      wrong.textContent = "Wrong!";
+    }
+    setTimeout(() => {
+      var correct = document.querySelector(".correct");
+      var wrong = document.querySelector(".incorrect");
+      correct.textContent = "";
+      wrong.textContent = "";
+    }, 1500);
+
+    getNextQstn();
+  });
+});
+onclick = countdown();
 startQuiz();
-
-
-
-
